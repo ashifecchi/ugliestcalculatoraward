8905a242a20229bafb2a91940e0cfbdbf07114ba
BT_1N3_32 <@>
2023-11-27 10:45:33 -0500

    sdfcasas

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,14 +1,25 @@
+/** This class is for calculating all the stuffs.
+ * @author AshiFecchi */
 public class LinearEquation {
-
+    /**Instance Variables:
+    *The first x*/
     private double x1;
+    /**The second x*/
     private double x2;
+    /**The first y*/
     private double y1;
+    /**The second y*/
     private double y2;
     //private double slope;
+    /**Temporary variable x*/
     private double tempx;
+    /**temporary variable y*/
     private double tempy;
 
 
+    /** The constructor class for an equation.
+     *
+     * */
     public LinearEquation(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
@@ -16,6 +27,7 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
+    /**Method to create the equation used for all calculations.*/
     public String CalculateEquation(){
         double rise = y2 - y1;
         double run = x2 - x1;
@@ -43,13 +55,16 @@ public class LinearEquation {
         }
         return "y=" + slope + "x-"+b;
     }
+    /**helper method to find B*/
     public double CalculateB(){
         return y1-(SlopeWhole()*x1);
     }
+    /**helper method to find slope if it is perfectly divisible*/
     public double SlopeWhole(){
         double slope = (double)((y2-y1)/(x2-x1));
         return slope;
     }
+    /**find slope as a string fraction*/
     public String SlopeFrac(){
         double rise = (y2-y1);
         double run = (x2-x1);
@@ -61,27 +76,33 @@ public class LinearEquation {
         }
         return rise+"/"+run;
     }
+    /**returms a string of the first point in coordinate form*/
     public String coord1(){
          tempx = RoundtoHundreds(x1);
          tempy = RoundtoHundreds(y1);
         return "("+tempx+", "+tempy+")";
     }
+    /**returns a string of the second point in coordinate form*/
     public String coord2(){
         tempx = RoundtoHundreds(x2);
         tempy = RoundtoHundreds(y2);
         return "("+tempx+", "+tempy+")";
     }
+    /**calculates distance between points*/
     public double Distance(){
         tempx = Math.pow(x2-x1,2);
         tempy= Math.pow(y2-y1,2);
         return RoundtoHundreds(Math.sqrt(tempx+tempy));
     }
+    /**method to print the line info*/
     public String lineInfo(){
         return "The two points are: " + coord1() +" and " + coord2() + "\nThe equation of the line between these points is: "+CalculateEquation()+"\nThe slope of this line is: "+SlopeWhole()+"\nThe y-intercept of this line is:"+ CalculateB()+"\nThe distance between these points is: "+Distance();
     }
+    /**helper method for rounding to nearest hundreds*/
     public double RoundtoHundreds(double inp){
             return (double)Math.round(inp*100) / 100;
     }
+    /**method to calculate y based on user input using the created equation object*/
     public double calcy(double x){
         return (SlopeWhole()*x)+CalculateB();
     }
